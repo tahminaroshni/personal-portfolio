dd4b2600c2310c68844f25610fa99e53caf71c13
tahminaroshni <tahminatanni12@gmail.com>
2023-02-06 11:38:22 +0600

    custom cursor done

--- src/hooks/useCustomCursor.js
+++ src/hooks/useCustomCursor.js
@@ -1,12 +1,29 @@
+import { useEffect } from "react"
+
 export const useCustomCursor = (innerCursor, outerCursor) => {
-  document.addEventListener('mousemove', mousemoveHandler);
+  useEffect(() => {
+    document.addEventListener('mousemove', mousemoveHandler);
 
-  function mousemoveHandler(e) {
-    let x = e.clientX;
-    let y = e.clientY;
-    console.log(e.target, x, y);
+    function mousemoveHandler(e) {
+      let x = e.clientX;
+      let y = e.clientY;
 
-    innerCursor.current.style.left = `${x}px`;
-    outerCursor.current.style.top = `${y}px`;
-  }
+      innerCursor.current.style.left = `${x}px`;
+      innerCursor.current.style.top = `${y}px`;
+      outerCursor.current.style.left = `${x}px`;
+      outerCursor.current.style.top = `${y}px`;
+    }
+
+    const links = Array.from(document.querySelectorAll('a'));
+    // console.log(links);
+    links.forEach(link => {
+      link.addEventListener('mouseover', () => {
+        innerCursor.current.classList.add('grow');
+      })
+
+      link.addEventListener('mouseleave', () => {
+        innerCursor.current.classList.remove('grow');
+      })
+    })
+  }, [innerCursor, outerCursor])
 }
